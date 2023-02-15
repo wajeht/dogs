0c7d94183bd7b9bc2e7313ea71332e4d39f08568
wajeht <58354193+wajeht@users.noreply.github.com>
2023-02-14 22:37:52 -0600

    feat: added iamges

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,13 @@
 import express from 'express';
+import images from './utils/dogs-images.js';
 const page = express.Router();
 
-page.get('/', (req, res) => res.status(200).render('./home.html'));
 page.get('/health', (req, res) => res.status(200).json({ message: 'ok', date: new Date() }));
+
+page.get('/', (req, res) => {
+  res.status(200).render('./home.html', {
+    images,
+  });
+});
 
 export default page;
