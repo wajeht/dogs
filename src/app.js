a206c5c8ed39b5cd96e6eb339bcb7a8561984a69
wajeht <58354193+wajeht@users.noreply.github.com>
2023-02-14 22:56:45 -0600

    feat: disable caching

--- src/app.js
+++ src/app.js
@@ -27,12 +27,7 @@ app.use(helmet());
 app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.use(
-  express.static(path.resolve(path.join(process.cwd(), 'public')), {
-    maxAge: '24h',
-  }),
-);
+app.use(express.static(path.resolve(path.join(process.cwd(), 'public'))));
 
 app.engine('html', ejs.renderFile);
 app.set('view engine', 'html');
