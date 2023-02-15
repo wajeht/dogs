6b0e673d23236968ae538efd7f02f31ee0d202ca
wajeht <58354193+wajeht@users.noreply.github.com>
2023-02-14 20:47:11 -0600

    feat: CI and bunch of other buiild script

---
+++ .eslintrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    sourceType: 'module',
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'prettier'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {},
+};
