55dddf8812a9ce7b50d5fdf10f843e5f3ee810eb
wajeht <58354193+wajeht@users.noreply.github.com>
2023-02-15 23:54:10 -0600

    fix: lint err

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,6 @@ const page = express.Router();
 page.get('/health', (req, res) => res.status(200).json({ message: 'ok', date: new Date() }));
 
 page.get('/', (req, res) => {
-  throw new Error('xxxxx');
   res.status(200).render('./home.html', {
     images,
   });
